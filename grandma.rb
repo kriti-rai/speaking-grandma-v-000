24fe5afa5bfae5e841b1ea1f6eb2243250ac06ad
Kriti Rai <kriti.rai@gmail.com>
2018-01-01 02:12:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(shout)
-  if shout == shout.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif shout == "I LOVE YOU GRANDMA!"
+  if shout == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
+  elseif shout == shout.upcase
+      return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
